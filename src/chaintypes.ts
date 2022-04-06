466e660fa973c8e95afb0c6f7bd1bdc87f370bb1
RyuH1 <haichuan@ata.network>
2022-04-06 11:57:56 +0800

    Update chaintypes for option snapshot and proposal workspaces

--- src/chaintypes.ts
+++ src/chaintypes.ts
@@ -86,7 +86,7 @@ const definitions: OverrideBundleDefinition = {
         VotingPower: 'U256',
         DAOProposalState: {
           finalized: "bool",
-          snapshots: "Vec<U256>",
+          snapshots: "Vec<Option<U256>>",
           blacklisted: "bool",
           votes: "Vec<VotingPower>",
           pub_voters: "Option<IpfsHash>",
@@ -101,6 +101,7 @@ const definitions: OverrideBundleDefinition = {
           _start: 'u64',
           _end: 'u64',
           _frequency: 'Option<u64>',
+          _workspaces: 'Vec<Workspace>',
           state: 'DAOProposalState'
         },
         VoteUpdate: {
