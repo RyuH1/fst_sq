9381c626c2bd1e94675b7dd7ba2e5468300d93f3
RyuH1 <haichuan@ata.network>
2022-06-02 16:11:58 +0800

    Adding supports for full-featured subquery for ctx network

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,8 +1,8 @@
 import {SubstrateExtrinsic} from "@subql/types";
-import {Proposal, Project, CrossChainAccount, Protocol, Privacy, ProposalStatus, VotingFormat, Workspace, Strategy, SolidityStrategy, SubstrateStrategy} from "../types";
-import { ProjectId, ProposalId, DAOProposal, Project as DAOProject, CrossChainAccount as DAOCrossChainAccount, VoteUpdate, Workspace as DAOWorkspace, Strategy as DAOStrategy } from "../interfaces/daoPortal/types"
+import {Proposal, Project, CrossChainAccount, Protocol, VotingFormat, Workspace, Strategy} from "../types";
+import { ProjectId, ProposalId, DAOProposal, Project as DAOProject, UserGroup, CrossChainAccount as DAOCrossChainAccount, VoteUpdate, Workspace as DAOWorkspace, Strategy as DAOStrategy } from "../interfaces/daoPortal/types"
 import fetch from "cross-fetch";
-import type { Vec } from '@polkadot/types';
+import type { Vec, Option, U256 } from '@polkadot/types';
 
 const IPFS_PIN_URL = "https://anydao.mypinata.cloud/ipfs";
 
@@ -59,112 +59,181 @@ export async function handleAddProposal(extrinsic: SubstrateExtrinsic): Promise<
     const projectId = project_id as ProjectId;
     const proposalId = proposal_id as ProposalId;
 
-    let record = new Proposal(`${projectId}-${proposalId}`);
-
-    record.projectId = projectId.toString();
-    record.proposal = proposalId.toNumber();
-
     const {extrinsic: {method: {args: [, proposal]}}} = extrinsic;
 
     const daoProposal = proposal as DAOProposal;
     logger.info(`(${extrinsic.block.block.header.number.toNumber()}) Create Proposal (project: ${projectId}, id: ${proposalId}):\n ${daoProposal}`);
 
     /// populating record
+    /// id
+    let record = new Proposal(`${projectId}-${proposalId}`);
+    
+    /// project
+    record.projectId = projectId.toString();
+    
+    /// proposal
+    record.proposal = proposalId.toNumber();
+
+    /// start
     record.start = daoProposal._start.toBigInt();
+
+    /// end
     record.end = daoProposal._end.toBigInt();
 
+    /// author
     await ensureCrossChainAccount(daoProposal._author);
     record.authorId = daoProposal._author.inner.toString();
 
-    record.privacy = daoProposal._privacy.type as Privacy;
+    /// privacy
+    switch(daoProposal._privacy.type) {
+        case 'Opaque': {
+            record.privacy = {
+                opaque: daoProposal._privacy.asOpaque.toNumber()
+            }
+            break;
+        }
+        case 'Rank': {
+            record.privacy = {
+                rank: ''
+            }
+            break;
+        }
+        case 'Private': {
+            record.privacy = {
+                private: ''
+            }
+            break;
+        }
+        case 'Public': {
+            record.privacy = {
+                public: ''
+            }
+            break;
+        }
+        case 'Mixed': {
+            record.privacy = {
+                mixed: ''
+            }
+            break;
+        }
+        default: {
+            record.privacy = {}
+            break;
+        }
+    }
 
+    /// format
     record.format = daoProposal._voting_format.type as VotingFormat;
 
+    /// optioncount
     record.optioncount = daoProposal._option_count.toNumber();
 
+    /// frequency
     if (daoProposal._frequency.isSome) {
         record.frequency = daoProposal._frequency.unwrap().toBigInt();
     }
 
-    // record.status = daoProposal.state.status.type as ProposalStatus;
+    /// finalized
+    record.finalized = false;
 
+    /// votes
     record.votes = [];
-
-    for (const vote of daoProposal.state.votes) {
-        record.votes.push(vote.toHex());
+    for (let i = 0; i < record.optioncount; i++) {
+        record.votes.push('0');
     }
 
-    if (daoProposal.state.pub_voters.isSome) {
-        record.pubvote = daoProposal.state.pub_voters.unwrapOrDefault().toString();
-    }
+    /// pubvote
+    /// skip
+    
+    /// blacklisted
+    record.blacklisted = false;
 
-    logger.info(`proposal: ${daoProposal}`)
+    /// created
+    record.created = extrinsic.block.block.header.number.toNumber();
 
+    /// updated
+    record.updated = extrinsic.block.block.header.number.toNumber();
+
+    /// data
     const data = daoProposal._data.toString();
-
     record.data = data;
 
+    /// title
+    /// description
+    /// options
     await updateProposalIpfs(record).then(async (response) => {
         record = response;
     }).catch((e) => {
         logger.error(`updateProposalIpfs error: ${e}`);
     })
 
-    record.created = extrinsic.block.block.header.number.toNumber();
-    record.updated = extrinsic.block.block.header.number.toNumber();
+    /// workspaces
+    const proj_record = await Project.get(`${projectId}`);
+    record.workspaces = proj_record.workspaces;
+    
+    /// snapshots
+    /// skip
+
+    logger.info(`proposal: ${daoProposal}`)
 
     await record.save();
 
     // update project
-    const proj_record = await Project.get(`${projectId}`);
+    // const proj_record = await Project.get(`${projectId}`);
     proj_record.prop_count++;
+    proj_record.prop_updated = extrinsic.block.block.header.number.toNumber();
+
     await proj_record.save();
 }
 
-async function populateStrategy(strategies: Vec<DAOStrategy>, workspaceId: string): Promise<void> {
+function populateStrategy(strategies: Vec<DAOStrategy>): Strategy[] {
+    let ret = [];
+
     for (let i = 0; i < strategies.length; i++) {
         const strategy = strategies.at(i);
 
-        let stg_record = new Strategy(`${workspaceId}-${i}`);
-        stg_record.workspaceId = workspaceId;
-        stg_record.protocol = strategy.type as Protocol;
+        let j_stg: Strategy = {
+            protocol: strategy.type,
+        }
 
         if (strategy.isSolidity) {
-            stg_record.solidity = strategy.asSolidity.type as SolidityStrategy;
+            j_stg.solidity = strategy.asSolidity.type;
         } else if (strategy.isSubstrate) {
-            stg_record.substrate = strategy.asSubstrate.type as SubstrateStrategy;
+            j_stg.substrate = strategy.asSubstrate.type;
         } else {
             // shouldn't happen
         }
 
         if (!strategy.isEmpty) {
-            stg_record.param = strategy.toHex();
-        }
-        
-        await stg_record.save();
+            j_stg.param = strategy.toHex();
+        }        
+
+        ret.push(j_stg)
     }
+
+    return ret;
 }
 
-async function populateWorkspace(workspaces: Vec<DAOWorkspace>, projectId: ProjectId): Promise<void>  {
-
+function populateWorkspace(record: Project, workspaces: Vec<DAOWorkspace>): Project  {
+    record.workspaces = [];
 
     for (let i = 0; i < workspaces.length; i++) {
         const workspace = workspaces.at(i);
         const chainId = workspace._chain.toNumber();
-        const wid = `${projectId}-${chainId}`;
-        let ws_record = new Workspace(wid);
-        ws_record.projectId = projectId.toString();
-        ws_record.chain = chainId;
 
-        await ws_record.save();
-
-        await populateStrategy(workspace.strategies, wid)
+        let j_workspace: Workspace = {
+            chain: chainId,
+            strategies: populateStrategy(workspace.strategies)
+        }
         
-        
-
+        record.workspaces.push(j_workspace);
     }
 
+    return record;
+}
 
+async function constructUserGroup(record: Project, usergroup: UserGroup): Promise<Project> {
+    return record;
 }
 
 async function updateProjectIpfs(record: Project): Promise<Project> {
@@ -196,20 +265,45 @@ export async function handleAddProject(extrinsic: SubstrateExtrinsic): Promise<v
     const {event: {data: [project_id]}} = addEvent;
     const projectId = project_id as ProjectId;
 
-    let record = new Project(`${projectId}`);
-
     const {extrinsic: {method: {args: [project]}}} = extrinsic;
     const daoProject = project as DAOProject;
 
     logger.info(`(${extrinsic.block.block.header.number.toNumber()}) Create Project (id: ${projectId}):\n ${daoProject}`);
-    
+
+    let record = new Project(`${projectId}`);
+
+    /// populating record
+    /// owner
     await ensureCrossChainAccount(daoProject.usergroup.owner);
     record.ownerId = daoProject.usergroup.owner.inner.toString();
-    const data = daoProject.data.toString();
-    record.data = data;
+
+    /// updated
     record.updated = extrinsic.block.block.header.number.toNumber();
+    
+    /// prop_count
     record.prop_count = 0;
 
+    /// prop_updated
+    record.prop_updated = extrinsic.block.block.header.number.toNumber();
+
+    /// workspaces
+    record = populateWorkspace(record, daoProject.workspaces);
+
+    /// enable_proposer
+    await constructUserGroup(record, daoProject.usergroup).then(async (response) => {
+        record = response
+    }).catch((e) => {
+        logger.error(`constructUserGroup error: ${e}`);
+    })
+
+    /// data
+    const data = daoProject.data.toString();
+    record.data = data;
+
+    /// name
+    /// description
+    /// icon
+    /// banner
     await updateProjectIpfs(record).then(async (response) => {
         record = response;
     }).catch((e) => {
@@ -217,8 +311,6 @@ export async function handleAddProject(extrinsic: SubstrateExtrinsic): Promise<v
     })
 
     await record.save();
-
-    await populateWorkspace(daoProject.workspaces, projectId);
 }
 
 export async function handleUpdateProject(extrinsic: SubstrateExtrinsic): Promise<void> {
@@ -237,6 +329,8 @@ export async function handleUpdateProject(extrinsic: SubstrateExtrinsic): Promis
     record.data = data;
     record.updated = extrinsic.block.block.header.number.toNumber();
 
+    record = populateWorkspace(record, daoProject.workspaces);
+
     await updateProjectIpfs(record).then(async (response) => {
         record = response;
     }).catch((e) => {
@@ -245,18 +339,18 @@ export async function handleUpdateProject(extrinsic: SubstrateExtrinsic): Promis
 
     await record.save();
 
-    const ws_records = await Workspace.getByProjectId(projectId.toString());
-    for (let i = 0; i < ws_records.length; i++) {
-        const wid = ws_records.at(i).id;
-        const stg_records = await Strategy.getByWorkspaceId(wid);
-        for (let j = 0; j < stg_records.length; j++) {
-            const sid = stg_records.at(j).id;
-            await Strategy.remove(sid);
-        }
-        await Workspace.remove(wid);
-    }
+    // const ws_records = await Workspace.getByProjectId(projectId.toString());
+    // for (let i = 0; i < ws_records.length; i++) {
+    //     const wid = ws_records.at(i).id;
+    //     const stg_records = await Strategy.getByWorkspaceId(wid);
+    //     for (let j = 0; j < stg_records.length; j++) {
+    //         const sid = stg_records.at(j).id;
+    //         await Strategy.remove(sid);
+    //     }
+    //     await Workspace.remove(wid);
+    // }
 
-    await populateWorkspace(daoProject.workspaces, projectId);
+    // await populateWorkspace(daoProject.workspaces, projectId);
 }
 
 export async function handleUpdateVote(extrinsic: SubstrateExtrinsic): Promise<void> {
@@ -280,11 +374,34 @@ export async function handleUpdateVote(extrinsic: SubstrateExtrinsic): Promise<v
 
     const timestamp = extrinsic.block.timestamp;
 
-    // if (timestamp.getTime() >= record.end) {
-    //     record.status = ProposalStatus.Closed;
-    // } else {
-    //     record.status = ProposalStatus.Ongoing;
-    // }
+    record.updated = extrinsic.block.block.header.number.toNumber();
+
+    await record.save();
+}
+
+export async function handleUpdateSnapshots(extrinsic: SubstrateExtrinsic): Promise<void> {
+    const {extrinsic: {method: {args: [project_id, proposal_id, snapshots]}}} = extrinsic;
+
+    const projectId = project_id as ProjectId;
+    const proposalId = proposal_id as ProposalId;
+    const vec_snapshots = snapshots as Vec<Option<U256>>;
+
+    logger.info(`(${extrinsic.block.block.header.number.toNumber()}) Update Snapshots (project: ${projectId}, proposal: ${proposalId}):\n ${vec_snapshots}`);
+
+    const record = await Proposal.get(`${projectId}-${proposalId}`);
+
+    record.snapshots = [];
+
+    for (const snapshot of vec_snapshots) {
+        // record.votes.push(power.toHex());
+        if (snapshot.isNone) {
+            record.snapshots.push('');
+        } else {
+            record.snapshots.push(snapshot.toHex());
+        }
+    }
+
+    const timestamp = extrinsic.block.timestamp;
 
     record.updated = extrinsic.block.block.header.number.toNumber();
 
